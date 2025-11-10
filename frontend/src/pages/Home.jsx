f44a338f9a5fe6a871cbdbba491835ae71ce1190
Anshuman singh <anshumansingh891700@gmai.com>
2025-11-10 12:41:52 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,16 +17,16 @@ We connect students and management securely and transparently.</h3>
         <div className='flex flex-col gap-12'>
           <div className='flex items-center '>
             <div className='rounded-full overflow-hidden z-10'><img className='w-44 h-44 object-cover rounded-full' src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000" alt="" /></div>
-            <div className='border-2 rounded-2xl w-[75%] h-32 relative right-11'></div>
+            <div className='border-4 rounded-2xl w-[75%] h-25 relative right-11 bg-stone-400'></div>
         </div>
         <div className='flex items-center relative left-20'>
             
-            <div className='border-2 rounded-2xl w-[75%] h-32 relative left-11'></div>
+            <div className='border-4 rounded-2xl w-[75%] h-25 relative left-11 bg-stone-400'></div>
             <div className='rounded-full overflow-hidden z-10'><img className='w-44 h-44 object-cover rounded-full' src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000" alt="" /></div>
         </div>
         <div className='flex items-center '>
             <div className='rounded-full overflow-hidden z-10'><img className='w-44 h-44 object-cover rounded-full' src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000" alt="" /></div>
-            <div className='border-2 rounded-2xl w-[75%] h-32 relative right-11'></div>
+            <div className='border-4 rounded-2xl w-[75%] h-25 relative right-11 bg-stone-400'></div>
         </div>
         </div>
       </div>
