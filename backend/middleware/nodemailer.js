dbfa7ac518bb4338c9a6d14b853e04031bf4d790
anmolvats001 <vatsanmol4@gmail.com>
2025-12-22 16:43:29 +0530

    hello

--- backend/middleware/nodemailer.js
+++ backend/middleware/nodemailer.js
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
